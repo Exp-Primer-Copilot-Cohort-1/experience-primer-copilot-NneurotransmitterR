4f9be5daae0b6d8210131b46a0db252ca8c026ee
Zhao Kejie <47296255+NneurotransmitterR@users.noreply.github.com>
2023-10-18 05:54:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,53 @@
+function skillsMember() {
+    var skills = [
+        {
+            "name": "HTML",
+            "value": "95%"
+        },
+        {
+            "name": "CSS",
+            "value": "90%"
+        },
+        {
+            "name": "JavaScript",
+            "value": "85%"
+        },
+        {
+            "name": "jQuery",
+            "value": "80%"
+        },
+        {
+            "name": "PHP",
+            "value": "80%"
+        },
+        {
+            "name": "MySQL",
+            "value": "75%"
+        },
+        {
+            "name": "Python",
+            "value": "70%"
+        },
+        {
+            "name": "C",
+            "value": "65%"
+        },
+        {
+            "name": "C++",
+            "value": "60%"
+        },
+        {
+            "name": "Java",
+            "value": "55%"
+        },
+        {
+            "name": "Swift",
+            "value": "50%"
+        },
+        {
+            "name": "C#",
+            "value": "45%"
+        }
+    ];
+    return skills;
+}
